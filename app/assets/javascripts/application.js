7e3827c95c4dcd88be53f5894db49680c66da412
Kenneth Mui <physkennethmui@gmail.com>
2014-10-05 22:12:59 -0400

    Still problems with signout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+
 //= require jquery_ujs
-//= require twitter/bootstrap
-//= require turbolinks
+
+//= require bootstrap
+
 //= require_tree .
